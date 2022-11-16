6a236ca24fb826a3e32ac2936865a2e964b8875c
alex <alex.jiuk.kim@mgmail.com>
2022-11-16 16:20:14 +0900

    Add .env

---
+++ backend/src/regions/createRegionDto.ts
@@ -0,0 +1,4 @@
+export class CreateRegionDto {
+  logitude: string;
+  latitude: string;
+}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: User[] = [];
+
+  createUser(user: User) {
+    this.users.push(user);
+  }
+
+  findAllUsers() {
+    return this.users;
+  }
+}
