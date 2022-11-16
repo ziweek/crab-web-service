bdd563ae1765e1c09f7e8786417a7b54a9d44348
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2022-11-16 16:31:51 +0900

    Apply .gitignore

--- backend/dist/users/users.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { CreateUserDto } from './createUserDto';
-export declare class UsersController {
-    findAllUsers(): Promise<any[]>;
-    findOneUser(param: any): Promise<any[]>;
-    createUser(createUserDto: CreateUserDto): Promise<any[]>;
-    updateUser(param: any): Promise<any[]>;
-    deleteUser(param: any): Promise<any[]>;
-}
