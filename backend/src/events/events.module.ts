2c5c3731ee055e9c5693dcb449cfcdd94bdb53cf
alex <alex.jiuk.kim@mgmail.com>
2022-11-30 14:21:21 +0900

    npm build

--- backend/src/events/events.module.ts
+++ backend/src/events/events.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
+import { EventsService } from './events.service';
+import { EventsController } from './events.controller';
 
 @Module({
-  providers: [EventsGateway],
+  providers: [EventsGateway, EventsService],
+  controllers: [EventsController],
 })
 export class EventsModule {}
