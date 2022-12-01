d46348c9692657936c75bdd4181537657c0f2675
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2022-12-01 12:29:59 +0900

    build

--- frontend/pages/intro/index.tsx
+++ frontend/pages/intro/index.tsx
@@ -4,10 +4,36 @@ import type { NextPage } from "next";
 import Logo from "asset/image/logo.png";
 import { useRouter } from "next/router";
 import * as S from "styles/intro/style";
+import { useRecoilState } from "recoil";
+import { userToken, userInfo } from "components/states";
+import axios from "axios";
 const Intro: NextPage = () => {
+  const [token, setToken] = useRecoilState(userToken);
+  const [user, setUser] = useRecoilState(userInfo);
+  const getUser = async () => {
+    console.log("getuser start", token);
+    await axios
+      .get(`${process.env.BASE_URL}` + "/auth/authenticate", {
+        headers: { Authorization: "Bearer " + token },
+      })
+      .then((response) => {
+        console.log("getUser", response);
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
   const router = useRouter();
   const move = () => {
-    router.push("/main");
+    if (!user) {
+      router.push("signin");
+    } else {
+      router.push("/");
+    }
   };
   setTimeout(move, 3000);
   return (
